f1b09ff6c9e840819e2f5323789d2e458348cec1
Alex Wong <likhang.wong97@gmail.com>
2025-11-12 10:55:40 -0800

    keeping forecasts and analytics only

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-sales_monitor_page = st.Page("streamlit_pages/prediction.py", title="Forecasts", icon=":material/finance_mode:")
+forecasts_page = st.Page("streamlit_pages/prediction.py", title="Forecasts", icon=":material/finance_mode:")
 build_your_own_page = st.Page("streamlit_pages/build_your_own.py", title="Build Your Own Forecast", icon=":material/finance_mode:")
 analytics_page = st.Page("streamlit_pages/analytics_test.py", title="Sales analytics", icon=":material/finance_mode:")
 diagnostics_total_page = st.Page("streamlit_pages/diagnostics_net_sales.py", title="Model diagnostics - Net Sales", icon=":material/monitor_heart:")
@@ -10,6 +10,5 @@ diagnostics_item_A_page = st.Page("streamlit_pages/diagnostics_item_A.py", title
 diagnostics_item_B_page = st.Page("streamlit_pages/diagnostics_item_B.py", title="Model diagnostics - Soft serve Sales", icon=":material/monitor_heart:")
 diagnostics_orders_page = st.Page("streamlit_pages/diagnostics_orders.py", title="Model diagnostics - In Store Orders", icon=":material/monitor_heart:")
 
-pg = st.navigation({"Welcome to SweetPulse!":[sales_monitor_page, build_your_own_page, analytics_page, diagnostics_total_page, diagnostics_item_A_page, diagnostics_item_B_page,
-                                      diagnostics_orders_page]})
+pg = st.navigation({"Welcome to SweetPulse!":[forecasts_page, analytics_page]})
 pg.run()
